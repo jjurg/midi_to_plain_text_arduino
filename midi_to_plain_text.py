3c3cf04c0a1fd0bd0f38c2818a4635da6b3131da
jjurg <jacco.jurg@gmail.com>
2024-10-19 23:23:07 +0200

    arduino support added

--- midi_to_plain_text.py
+++ midi_to_plain_text.py
@@ -29,11 +29,25 @@ midi_note_dict = {
     108: 'C8'
 }
 
+arduino_note_dict = {
+    0: 'NOTE_REST',
+    21: 'NOTE_A0', 22: 'NOTE_AS0', 23: 'NOTE_B0',
+    24: 'NOTE_C1', 25: 'NOTE_CS1', 26: 'NOTE_D1', 27: 'NOTE_DS1', 28: 'NOTE_E1', 29: 'NOTE_F1', 30: 'NOTE_FS1', 31: 'NOTE_G1', 32: 'NOTE_GS1', 33: 'NOTE_A1', 34: 'NOTE_AS1', 35: 'NOTE_B1',
+    36: 'NOTE_C2', 37: 'NOTE_CS2', 38: 'NOTE_D2', 39: 'NOTE_DS2', 40: 'NOTE_E2', 41: 'NOTE_F2', 42: 'NOTE_FS2', 43: 'NOTE_G2', 44: 'NOTE_GS2', 45: 'NOTE_A2', 46: 'NOTE_AS2', 47: 'NOTE_B2',
+    48: 'NOTE_C3', 49: 'NOTE_CS3', 50: 'NOTE_D3', 51: 'NOTE_DS3', 52: 'NOTE_E3', 53: 'NOTE_F3', 54: 'NOTE_FS3', 55: 'NOTE_G3', 56: 'NOTE_GS3', 57: 'NOTE_A3', 58: 'NOTE_AS3', 59: 'NOTE_B3',
+    60: 'NOTE_C4', 61: 'NOTE_CS4', 62: 'NOTE_D4', 63: 'NOTE_DS4', 64: 'NOTE_E4', 65: 'NOTE_F4', 66: 'NOTE_FS4', 67: 'NOTE_G4', 68: 'NOTE_GS4', 69: 'NOTE_A4', 70: 'NOTE_AS4', 71: 'NOTE_B4',
+    72: 'NOTE_C5', 73: 'NOTE_CS5', 74: 'NOTE_D5', 75: 'NOTE_DS5', 76: 'NOTE_E5', 77: 'NOTE_F5', 78: 'NOTE_FS5', 79: 'NOTE_G5', 80: 'NOTE_GS5', 81: 'NOTE_A5', 82: 'NOTE_AS5', 83: 'NOTE_B5',
+    84: 'NOTE_C6', 85: 'NOTE_CS6', 86: 'NOTE_D6', 87: 'NOTE_DS6', 88: 'NOTE_E6', 89: 'NOTE_F6', 90: 'NOTE_FS6', 91: 'NOTE_G6', 92: 'NOTE_GS6', 93: 'NOTE_A6', 94: 'NOTE_AS6', 95: 'NOTE_B6',
+    96: 'NOTE_C7', 97: 'NOTE_CS7', 98: 'NOTE_D7', 99: 'NOTE_DS7', 100: 'NOTE_E7', 101: 'NOTE_F7', 102: 'NOTE_FS7', 103: 'NOTE_G7', 104: 'NOTE_GS7', 105: 'NOTE_A7', 106: 'NOTE_AS7', 107: 'NOTE_B7',
+    108: 'NOTE_C8'
+}
+
 midi = MidiFile(filename)
 
 notes = []
 current_notes = {}
 clocks_per_quarter = midi.ticks_per_beat
+print(clocks_per_quarter)
 
 print("Parsing MIDI file...")
 for track in midi.tracks:
@@ -46,15 +60,18 @@ for track in midi.tracks:
             if msg.note in current_notes:
                 start_time = current_notes.pop(msg.note)
                 duration = current_time - start_time
-                note = midi_note_dict.get(msg.note, 'unknown')
+                note_dict = arduino_note_dict if output_format == 'arduino' else midi_note_dict
+                note = note_dict.get(msg.note, 'unknown')
                 notes.append((note, duration))
                 if note == 'unknown':
                     print(f"Warning: Missing note for MIDI value {msg.note}")
 
 # Now notes is in the format (A4, 256), (C5, 512) for example
 
-# Now we just need to convert from clocks to note type
 note_length_dict = {
+    clocks_per_quarter / 128: 'onettwohundredandfithysixedNote',
+    clocks_per_quarter / 64: 'onettwohundredandfithysixedNote',
+    clocks_per_quarter / 32: 'onehundredandtwentyeightNote',
     clocks_per_quarter / 16: 'sixtyfourthNote',
     clocks_per_quarter / 8: 'thirtysecondNote',
     clocks_per_quarter / 4: 'sixteenthNote',
@@ -66,18 +83,18 @@ note_length_dict = {
     clocks_per_quarter * 16: 'quadruplewholeNote'
 }
 
-# Change the second entry to the corresponding dictionary value
-notes = [(note[0], note_length_dict.get(note[1], 'unknown')) for note in notes]
+# Change the second entry to the corresponding dictionary value for text output
+notes_text = [(note[0], note_length_dict.get(note[1], 'unknown - ' + str(note[1]))) for note in notes]
 
-for note, duration in notes:
-    if duration == 'unknown':
-        print(f"Warning: Missing duration for clock value {note[1]}")
+for note, duration in notes_text:
+    if 'unknown' in duration:
+        print(f"Warning: Missing duration for clock value {duration}")
 
 if output_format == 'text':
     print("Writing to file...")
     with open("plain_{}.txt".format(filename), 'w') as f:
         # [1:-1] to avoid the brackets of the list
-        note_string = str(notes)[1:-1]
+        note_string = str(notes_text)[1:-1]
         note_string = note_string.replace("'", "")
         f.write(note_string)
     print("Successfully saved the converted file to {}".format("plain_{}.txt".format(filename)))
@@ -87,23 +104,23 @@ elif output_format == 'arduino':
     melody = []
     noteDurations = []
 
-    for note, duration in notes:
-        if note != 'rest':
-            melody.append(f'NOTE_{note}')
+    for note, duration in notes[:256]:
+        if note != 'NOTE_REST':
+            melody.append(note)
         else:
             melody.append('0')
-        noteDurations.append(str(duration).replace('sixtyfourthNote', '64')
-                                        .replace('thirtysecondNote', '32')
-                                        .replace('sixteenthNote', '16')
-                                        .replace('eighthNote', '8')
-                                        .replace('quarterNote', '4')
-                                        .replace('halfNote', '2')
-                                        .replace('wholeNote', '1'))
+        noteDurations.append(str(4*int(clocks_per_quarter / duration)))
 
     with open("arduino_{}.ino".format(filename), 'w') as f:
-        f.write("int melody[] = {\n  " + ', '.join(melody) + "\n};\n\n")
+        f.write("int melody[] = {\n")
+        for i in range(0, len(melody), 8):
+            f.write("  " + ', '.join(melody[i:i+8]) + ",\n")
+        f.write("};\n\n")
         f.write("// note durations: 4 = quarter note, 8 = eighth note, etc.:\n")
-        f.write("int noteDurations[] = {\n  " + ', '.join(noteDurations) + "\n};\n")
+        f.write("int noteDurations[] = {\n")
+        for i in range(0, len(noteDurations), 8):
+            f.write("  " + ', '.join(noteDurations[i:i+8]) + ",\n")
+        f.write("};\n")
     print("Successfully saved the converted file to {}".format("arduino_{}.ino".format(filename)))
 else:
     print("Invalid output format. Use 'text' or 'arduino'.")
